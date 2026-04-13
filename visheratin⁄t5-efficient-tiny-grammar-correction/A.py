f37dec97ad2cb86276d09b999265a66642ee384c
Dimpho Magoro <dimphomagoro5@gmail.com>
2026-04-13 05:10:05 +0000

    Simple Refactoring

---
+++ visheratin⁄t5-efficient-tiny-grammar-correction/A.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Point to the local virtual folder we defined in TOML
+model_path = "./model"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSeq2SeqLM.from_path(model_path)
+
+def fix_it(input_text):
+    prompt = f"gec: {input_text}"
+    inputs = tokenizer(prompt, return_tensors="pt")
+    
+    with torch.no_grad():
+        generated = model.generate(**inputs, max_new_tokens=50, num_beams=5)
+    
+    return tokenizer.decode(generated[0], skip_special_tokens=True)
