99b9c5cfbd96ddf29465d7b415235a06323f6860
Dimpho Magoro <dimphomagoro5@gmail.com>
2026-04-13 05:24:56 +0000

    FIx

--- visheratin⁄t5-efficient-tiny-grammar-correction/A.py
+++ visheratin⁄t5-efficient-tiny-grammar-correction/A.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 # Point to the local virtual folder we defined in TOML
 model_path = "./model"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForSeq2SeqLM.from_path(model_path)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
 def fix_it(input_text):
     prompt = f"gec: {input_text}"
